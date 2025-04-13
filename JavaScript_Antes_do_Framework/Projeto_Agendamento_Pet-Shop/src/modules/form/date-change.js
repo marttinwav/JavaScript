395c515baa6a62cd0065138578dfe2710ad1110a
MarttinWav <osmar.martinjunior123@gmail.com>
2025-04-13 17:37:44 -0300

    Delete JavaScript_Antes_do_Framework/Projeto_Agendamento_Pet-Shop directory

--- JavaScript_Antes_do_Framework/Projeto_Agendamento_Pet-Shop/src/modules/form/date-change.js
+++
@@ -1,8 +0,0 @@
-import { schedulesDay } from "../schedules/load";
-
-// Seleciona o input de data
-const [select1, select2] = document.getElementsByClassName("date");
-
-// Recarrega a lista de horários quando o input de data mudar.
-select1.onchange = () => schedulesDay();
-select2.onchange = () => schedulesDay();
